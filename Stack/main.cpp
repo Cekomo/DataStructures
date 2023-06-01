caef11deec51d328edf09de6fe09b6bef469de9a
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-01 23:59:08 +0300

    invoke class instance methods in main.cpp

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,6 +1,20 @@
 #include "Stack.h"
 
 int main() {
+    Stack stack;
+
+    stack.Push(15);
+    stack.Push(31);
+    stack.Push(57);
+
+    stack.Pop();
+
+    std::cout << "Stack size: " << stack.GetSize() << " Latest Node: " << stack.Peek() << std::endl;
+    std::cout << "Stack is empty: " << stack.IsEmpty() << std::endl;
+
+    stack.Clear();
+
+    std::cout << "Stack size: " << stack.GetSize() << std::endl;
 
     return 0;
 }
