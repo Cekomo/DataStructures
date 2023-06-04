eff0749ad9a7f447904198b34cb94716e56454ca
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-04 12:49:14 +0300

    implement classes of hash table

---
+++ HashTable/HashTable.h
@@ -0,0 +1,15 @@
+#ifndef DATASTRUCTURES_HASHTABLE_H
+#define DATASTRUCTURES_HASHTABLE_H
+
+#include <iostream>
+
+class HashTable {
+private:
+
+
+public:
+
+
+};
+
+#endif //DATASTRUCTURES_HASHTABLE_H
