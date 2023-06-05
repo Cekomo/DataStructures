7865f390fcd83e8537fc59f35fbe5ddc177bfce7
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-06-05 23:01:47 +0300

    implement methods of Hash Table

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -1,1 +1,42 @@
 #include "HashTable.h"
+
+HashTable::HashTable() {
+    bucketSize = 10;
+    buckets = new Bucket[bucketSize];
+}
+
+void HashTable::Insert(int key, int data) {
+
+}
+
+int HashTable::GetValue(int key) {
+    return 0;
+}
+
+void HashTable::Remove(int key) {
+
+}
+
+bool HashTable::ContainsKey(int key) {
+    return false;
+}
+
+int HashTable::GetSize() {
+    return nodeSize;
+}
+
+bool HashTable::IsEmpty() {
+    return nodeSize == 0;
+}
+
+void HashTable::Clear() {
+
+}
+
+void HashTable::Resize(int newSize) {
+
+}
+
+HashTable::~HashTable() {
+    delete buckets; // delete buckets array
+}
---
+++ Queue/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by cemil on 5.06.2023.
+//
