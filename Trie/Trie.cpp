4a20f0fabeda63e402e87ec16f915ddfffbf379f
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-07-13 18:53:51 +0300

    intercommit

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -2,20 +2,39 @@
 
 Trie::Trie() = default;
 
+struct Node {
+    std::string letter;
+    Node *next[5];
+
+    explicit Node(std::string letter) {
+        this->letter = letter;
+        for (int i = 0; i < 5; i++)
+            next[i] = nullptr;
+    }
+};
+
 void Trie::Insert(char letter, std::string path, bool isConcluded) {
     Node *node = new Node(letter);
 
-    if (head == nullptr)
+    if (head == nullptr) {
         head = node;
-    else {
-        char c = path[0];
-        if (c != letter) return;
+        return;
+    }
 
-        for (std::size_t i = 0; i < path.length(); i++) {
-            c = path[i];
+    Node *currentNode = head;
+    char currentLetter = path[0];
+    if (currentLetter != head->letter) return;
 
+    for (std::size_t i = 0; i < path.length(); i++) { // p a t h
+        currentLetter = path[i];
+        for (int j = 0; j < 5 && &currentNode->next[j] == nullptr; j++) {
+            if (currentNode->next[j].letter == currentLetter) {
+                currentNode = &currentNode->next[j];
+                break;
+            }
         }
     }
+
 }
 
 Trie::~Trie() = default;
